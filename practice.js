219c928054abb37c6fa510c3c096ed2644221e36
zack alsiday <alsidayzack@gmail.com>
2022-10-28 11:23:58 -0700

    finished maxValue

--- practice.js
+++ practice.js
@@ -298,4 +298,147 @@
 
 ///////////////////////////////////////////////////////
 
+// Write a function `logEach(array)` that prints every element of the array and its
+// index to the console.
+//
+// Example:
+//
+// logEach(["Anthony", "John", "Carson"]); // prints
+// 0: Anthony
+// 1: John
+// 2: Carson
 
+// function logEach(array){
+//     for (i = 0; i < array.length; i++){
+//         console.log(i + ': ' + array[i])
+//     }
+// }
+
+// console.log(logEach(["Anthony", "John", "Carson"]))
+
+/////////////////////////////////////////////////////
+
+// Write a function range(start, end) that returns an array that contains all
+// numbers between 'start' and 'end' in sequential order.
+//
+// Examples:
+//
+// range(1,4) => [1,2,3,4]
+// range(4,2) => []
+
+// function range(start, end){
+//     let array = [] 
+//     for (i = start; i <= end; i++){
+//         array.push(i)
+//     }
+//     return array 
+// }
+
+// console.log(range(1, 4))
+// console.log(range(4, 2))
+
+///////////////////////////////////////////////////////////
+
+// Write a function sumArray(array) that takes in an array of numbers that returns the
+// total sum of them.
+//
+//
+// Examples:
+//
+// sumArray([5, 6, 4]); // => 15
+// sumArray([7, 3, 9, 11]); // => 30
+
+// function sumArray(array){
+//     let sum = 0;
+//     for(i = 0; i <= array.length - 1; i++){
+//         sum += array[i]
+//     }
+
+//     return sum 
+// }
+
+// console.log(sumArray([5, 6, 4]))
+// console.log(sumArray([7, 3, 9, 11]))
+
+//////////////////////////////////////////////////////////
+
+// Write a function `capWords(words)` that takes in an array of words and
+// returns a new array where every word is capitalized.
+//
+// Example:
+//
+// capWords(['hello', 'boOtCaMp', 'PREP!']); // => ['HELLO', 'BOOTCAMP', 'PREP!']
+
+
+// function capWords(words){
+//     let arr = []
+//     for (i = 0; i <= words.length - 1; i++){
+//         arr.push(words[i].toUpperCase())
+//     }
+//     return arr 
+// }
+
+// console.log(capWords(['hello', 'boOtCaMp', 'PREP!'])); // => ['HELLO', 'BOOTCAMP', 'PREP!'])
+///////////////////////////////////////////////////////////////
+
+
+// Write a function `wordPeriods(sentence)` that takes in a sentence and returns a new
+// sentence where every word has period after it.
+//
+// Examples:
+//
+// wordPeriods('hello world'); // => 'hello. world.'
+// wordPeriods('what is the weather today'); // => 'what. is. the. weather. today.'
+
+// function wordPeriods(sentence){
+//     let sentSplit = sentence.split(' ')
+//     let newSent = []
+//     i = 0 
+//     while (i <= sentSplit.length - 1){
+//         newSent.push(sentSplit[i] + '.')
+//         i += 1
+//     }
+//     return newSent
+// }
+
+// console.log(wordPeriods('what is the weather today'))
+
+/////////////////////////////////////////////////////////////////////
+
+// Write a function `maxValue(array)` that returns the largest value in `array`. [1,2,5]
+// Assume `array` is an array of numbers.
+//
+// Examples:
+//
+// maxValue([12, 6, 43, 2]); // => 43
+// maxValue([]); // => null
+// maxValue([-4, -10, 0.43]); // => 0.43 
+
+/
+
+///////////////////////////////////////////////////////////////////////////
+
+// Write a function `myIndexOf(array, target)` that takes in an array of
+// numbers and a target number as arguments. It should return the index value
+// of the target if it is present in the array or -1 if it is not present.
+//
+// CONSTRAINT: Do not use the indexOf method.
+//
+// Examples:
+//
+// myIndexOf([1,2,3,4],4) => 3
+// myIndexOf([5,6,7,8],2) => -1
+
+// function myIndexOf(array, target){
+//     let final = -1
+//     for (i = 0; i <= array.length - 1; i++){
+//         if (target === array[i]){
+//             return i 
+//         }
+//     }
+//     return final 
+// }
+
+// console.log(myIndexOf([5, 6, 7, 8], 2))
+
+////////////////////////////////////////////////////////////////////
