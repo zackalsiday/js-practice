7c12166cf76f9c52958c53abd42aa9cac3f876cb
zack alsiday <alsidayzack@gmail.com>
2022-10-27 20:11:43 -0700

    solved to sumNums

--- practice.js
+++ practice.js
@@ -103,3 +103,118 @@
 // console.log(logBetween(-1, 2))
 // console.log(logBetween(14, 6))
 // console.log(logBetween(4, 6))
+
+/////////////////////////////////////////////////////////
+
+// Write a function `logBetweenStepper(min, max, step)` that takes in 3 numbers as
+// parameters. The function should print out numbers between `min` and `max` at `step`
+// intervals. See the following examples.
+//
+// Examples:
+//
+// logBetweenStepper(5, 9, 1) // prints
+// 5
+// 6
+// 7
+// 8
+// 9
+//
+// logBetweenStepper(-10, 15, 5) // prints
+// -10
+// -5
+// 0
+// 5
+// 10
+// 15
+
+// function logBetweenStepper(min, max, step){
+//     for (i = min; i <= max; i += step){
+//         console.log(i)
+//     }
+// }
+
+// console.log(logBetweenStepper(5, 9, 1))
+// console.log(logBetweenStepper(-10, 15, 5))
+
+/////////////////////////////////////////////////////////
+
+// Write a function `printFives(max)` that prints out the multiples of 5 that are
+// less than max.
+//
+// Try to solve this in two ways, using a conditional (if) and without using a
+// conditional
+//
+// Example:
+//
+// printFives(20) // prints
+// 0
+// 5
+// 10
+// 15
+
+// function printFives(max){
+//     for (i = 0; i < max; i += 5){
+//         console.log(i)
+//     }
+// }
+
+// function printFives(max){
+//     for (i = 0; i < max; i += 5){
+//         console.log(i)
+//     }
+// }
+
+
+
+// console.log(printFives(20))
+
+//////////////////////////////////////////////////
+
+// Write a function `printReverse(min, max)` that print out all numbers from max
+// to min (exclusive), in reverse order.
+//
+// Examples
+//
+// printReverse(13, 18) // prints
+// 17
+// 16
+// 15
+// 14
+//
+// printReverse(90, 94) // prints
+// 93
+// 92
+// 91
+
+// function printReverse(min, max){
+//     for (i = max - 1; i > min; i--){
+//         console.log(i)
+//     }
+// }
+
+// console.log(printReverse(13,18))
+// console.log(printReverse(90, 94))
+
+////////////////////////////////////////////////
+
+// Write a function `sumNums(max)` that returns the sum of all positive numbers less
+// than or equal to max.
+//
+// Examples:
+//
+// sumNums(4); // => 10
+// sumNums(10); // => 55
+// sumNums(365); // => 66795
+
+// function sumNums(max){
+//     sum = 0
+//     for(i = 0; i <= max; i++){
+//         sum += i
+//     }
+//     return sum 
+// }
+
+// console.log(sumNums(4))
+// console.log(sumNums(10))
+// console.log(sumNums(365))
+
