a0b0d318567eb4e3bb4b274d8d3f69bf60599d18
zack alsiday <alsidayzack@gmail.com>
2022-10-27 19:46:58 -0700

    solved threeOrSeven

--- practice.js
+++ practice.js
@@ -34,10 +34,41 @@
 // plusFive(-8);  // => -3
 //
 
-function plusFive(num) {
-    return num + 5
-}
+// function plusFive(num) {
+//     return num + 5
+// }
 
-console.log(plusFive(10));  // => 15
-console.log(plusFive(2));   // => 7
-console.log(plusFive(-8));  // => -3
+// console.log(plusFive(10));  // => 15
+// console.log(plusFive(2));   // => 7
+// console.log(plusFive(-8));  // => -3
+
+/////////////////////////////////////////////////////////
+
+// Write a function `threeOrSeven` that takes in a number and returns `true`
+// if the number is divisible by either 3 or 7 and `false` otherwise.
+// Write two versions of this function, using conditionals and without using conditionals
+//
+//
+// Examples:
+// threeOrSeven(3);   // => true
+// threeOrSeven(42);  // => true
+// threeOrSeven(8);   // => false
+//
+// function threeOrSeven(num) {
+//     if (num % 3 === 0 || num % 7 === 0){
+//         return true 
+//     }else{
+//         return false 
+//     }
+// }
+
+// function threeOrSeven(num) {
+//     return num % 3 === 0 || num % 7 === 0
+// }
+
+
+
+// console.log(threeOrSeven(3));   // => true
+// console.log(threeOrSeven(42))
+// console.log(threeOrSeven(8))
+
