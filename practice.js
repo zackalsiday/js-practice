e417f56375b86cbcba67012621c95a326a17144c
zack alsiday <alsidayzack@gmail.com>
2022-10-29 18:48:53 -0700

    solved abbreviate

--- practice.js
+++ practice.js
@@ -856,3 +856,123 @@ fromMeToYou('love me or hate me') => 'love you or hate you'
 // }
 
 // console.log(evenCaps("the book is in front of the table"))
+
+///////////////////////////////////////////////////////////////
+
+/***********************************************************************
+Write a function `diffArrayLen(arr1, arr2)` that takes two arrays. The
+function should return true if the arrays have different lengths. The
+function should return false otherwise.
+
+Examples:
+
+var a1 = ['a', 'b', 'c'];
+var a2 = ['w', 'x', 'y'];
+var a3 = [1, 3, 7, 4];
+
+diffArrayLen(a1, a2); // => false
+diffArrayLen(a1, a3); // => true
+***********************************************************************/
+// var a1 = ['a', 'b', 'c'];
+// var a2 = ['w', 'x', 'y'];
+// var a3 = [1, 3, 7, 4];
+// function diffArrayLen(arr1, arr2) {
+//     return arr1.length !== arr2.length 
+// }
+
+// console.log(diffArrayLen(a1, a3))
+
+////////////////////////////////////////////////////////////////
+
+/***********************************************************************
+Write a function `mirrorArray(array)` that takes in an array as an
+argument and returns a new array "mirrored" as shown in the examples:
+
+Examples:
+
+mirrorArray([1,2,3]); // => [ 1, 2, 3, 3, 2, 1 ]
+mirrorArray(['a', 'b', 'c', 'd']); // => [ 'a', 'b', 'c', 'd', 'd', 'c', 'b', 'a' ]
+***********************************************************************/
+
+// function mirrorArray(array) {
+//     let newArr = array 
+//     for (i = array.length -1; i >= 0; i--){
+//         newArr.push(array[i])
+//     }
+//     return newArr
+// }
+
+// console.log(mirrorArray(['a', 'b', 'c', 'd']))
+
+//////////////////////////////////////////////////////////////////
+
+/***********************************************************************
+Write a function `avgValue(array)` that takes in an array of numbers and
+returns the average number.
+
+Examples:
+
+avgValue([10, 20]); // => 15
+avgValue([2, 3, 7]); // => 4
+avgValue([100, 60, 64]); // => 74.66666666666667
+***********************************************************************/
+
+// function avgValue(array) {
+//     let sum = 0;
+//     for (i = 0; i <= array.length - 1;i++){
+//         sum += array[i]
+//     }
+//     return sum / array.length 
+// }
+
+// console.log(avgValue([100, 60, 64]))
+
+/////////////////////////////////////////////////////////////
+
+/***********************************************************************
+Write a function `abbreviate(sentence)` that takes in a sentence string
+and returns a new sentence where words longer than 4 characters have
+their vowels removed. Assume the sentence has all lowercase characters.
+Feel free to use the array below in your solution:
+
+var vowels = ['a', 'e', 'i', 'o', 'u'];
+
+Examples:
+
+abbreviate('bootcamp prep is fun'); // => 'btcmp prep is fun'
+abbreviate('programming is fantastic'); // => 'prgrmmng is fntstc'
+abbreviate('hello world'); // => 'hll wrld'
+abbreviate('how are you'); // => 'how are you'
+***********************************************************************/
+
+// function abbreviate(sentence) {
+//     let splitSent = sentence.split(' ')
+//     let newSent = []
+//     for ( var i = 0; i < splitSent.length ;i++){
+
+//         if (splitSent[i].length >= 5){
+//             var word = splitSent[i]
+//             newSent.push(removeVowels(word))
+//         }else{
+//             newSent.push(word)
+//         }
+//     }
+//     return newSent.join(' ')
+// }
+
+// function removeVowels(word) {
+//     var vowels = ['a', 'e', 'i', 'o', 'u'];
+//     let sWord = word.split('')
+//     let newWord = []
+//     for ( var i = 0; i <= sWord.length - 1;i++){
+//         if (vowels.indexOf(sWord[i]) === -1){
+//             newWord.push(sWord[i])
+//         }
+//     }
+//     return newWord.join('')
+// }
+
+// console.log(abbreviate('hello world'))
+
+//////////////////////////////////////////////////////////////////////////////////////////////////
+
