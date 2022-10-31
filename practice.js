b01417b5af1e50418fe9fdeb341b8187cbd3b00e
zack alsiday <alsidayzack@gmail.com>
2022-10-31 15:21:32 -0700

    countInnerElement

--- practice.js
+++ practice.js
@@ -1494,4 +1494,50 @@ twoDimensionalTotal(arr2); // => 15
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+/***********************************************************************
+Write a function `countInnerElement(arr)` that takes in a 2-D array of elements.
+Each element of 'arr' is a sub array that contains multiple elements. The number
+of elements contained in each sub array are not the same. We can assume each sub
+array contains at least one element. We should return an object that counts
+how many times each element in each sub array repeats.
+// Examples:
+// var arr1 = [
+//   [1,2,4,5],
+//   [2,7,4],
+//   [1,4,5,2,7]
+// ]
+// countInnerElement(arr1) // => {1: 2, 2: 3, 4: 3, 5: 2, 7: 2}
+// var arr2 = [
+//   ['a','b','c','d'],
+//   ['a','b'],
+//   ['a','c','d','a']
+// ]
+// countInnerElement(arr2) // => {a: 4, b: 2, c: 2, d: 2}
+***********************************************************************/
+
+
+// function countInnerElement(arr) {
+//     let newObj = {};
+//     for (var a = 0; a <= arr.length -1; a++){
+//         for(var b = 0; b <= arr[a].length -1; b++){
+//             newObj[arr[a][b]] === undefined ? newObj[arr[a][b]] = 1 : newObj[arr[a][b]] += 1
+//         }
+//     }
+//     return newObj
+// }
+
+// var arr1 = [
+//   [1,2,4,5],
+//   [2,7,4],
+//   [1,4,5,2,7]
+// ]
+// console.log(countInnerElement(arr1)) // => {1: 2, 2: 3, 4: 3, 5: 2, 7: 2}
+// var arr2 = [
+//   ['a','b','c','d'],
+//   ['a','b'],
+//   ['a','c','d','a']
+// ]
+// console.log(countInnerElement(arr2)) // => {a: 4, b: 2, c: 2, d: 2}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
