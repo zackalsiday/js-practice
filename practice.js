a6435a751110371c53cdc1fc0f37810f6f8ccdd8
zack alsiday <alsidayzack@gmail.com>
2022-11-01 17:24:04 -0700

    fizzBuzz debugged

--- practice.js
+++ practice.js
@@ -1844,3 +1844,28 @@ titleize("shall we dance?", ["dance"])
 // }
 // console.log(isMagicNumer(26)) //=> true;
 // console.log(isMagicNumer(50)) //=> false;
+
+///////////////////////////////////////////////////////////////////////////////////////
+
+
+// Debug this function below so it passes mocha
+
+// Examples:
+//
+// fizzBuzz(19) //=> [3, 5, 6, 9, 10, 12, 18]
+
+function fizzBuzz(max) {
+    var i = 1;
+    var array = [];
+
+    while (i < max) {
+        if ((i % 5 === 0 || i % 3 === 0 ) && i !== 15) {
+            array.push(i);
+        }
+        i += 1;
+    }
+
+    return array;
+}
+
+console.log(fizzBuzz(19))
