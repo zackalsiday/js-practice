5e9d69cf88c681c1a38dc18d92e980eb6524c0bb
zack alsiday <alsidayzack@gmail.com>
2022-11-02 15:26:23 -0700

    completed gcf

--- practice.js
+++ practice.js
@@ -2317,3 +2317,27 @@ shortestWord('bootcamp prep') => 'prep'
 
 // console.log(shortestWord('app academy is cool'))
 // console.log(shortestWord('bootcamp prep'))
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/*******************************************************************************
+Write a function greatestCommonFactor(num1, num2) that returns the largest number
+that is divides both `num1` and `num2`.
+
+Examples:
+
+greatestCommonFactor(15, 25) => 5
+greatestCommonFactor(16, 24) => 8
+greatestCommonFactor(7, 11) => 1
+*******************************************************************************/
+
+function greatestCommonFactor(num1, num2) {
+    let start = Math.min(num1, num2)
+    for (var i = start; i > 0; i--){
+        if (num1 % i === 0 && num2 % i === 0) return i
+    }
+}
+
+console.log(greatestCommonFactor(15, 25))
+console.log(greatestCommonFactor(16, 24))
+console.log(greatestCommonFactor(7, 11)) 
