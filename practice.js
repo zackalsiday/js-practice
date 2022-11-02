6d76d3cec54e5543bff8a38f61b966bbdf839a61
zack alsiday <alsidayzack@gmail.com>
2022-11-01 18:24:54 -0700

    solved phraseFinder

--- practice.js
+++ practice.js
@@ -2052,9 +2052,43 @@ minMaxProduct([6, 3, 7, 2]) => 14
 minMaxProduct([0, 1, -5, 3, 6]) => -30
 *******************************************************************************/
 
-function minMaxProduct(array) {
-    return Math.min(...array) * Math.max(...array)
+// function minMaxProduct(array) {
+//     return Math.min(...array) * Math.max(...array)
+// }
+
+// console.log(minMaxProduct([6, 3, 7, 2]))
+// console.log(minMaxProduct([0, 1, -5, 3, 6]))
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/*******************************************************************************
+Write a function phraseFinder(words, phrase), that takes in an array of words and a
+string representing a phrase. The function should return true if the phrase can be
+formed by a pair of words from the array. The function should return false if the
+phrase cannot be formed by any pair of words.
+
+Examples:
+
+phraseFinder(['world', 'prep', 'hello', 'bootcamp'], 'bootcamp prep') => true
+phraseFinder(['world', 'bootcamp', 'hello', 'prep'], 'bootcamp prep') => true
+phraseFinder(['world', 'bootcamp', 'hello', 'prep'], 'hello world') => true
+phraseFinder(['world', 'bootcamp', 'hello', 'prep'], 'hello prep') => true
+phraseFinder(['world', 'bootcamp', 'hello', 'prep'], 'hello goodbye') => false
+*******************************************************************************/
+
+function phraseFinder(words, phrase) {
+    let splitPhrase = phrase.split(' ')
+    for (var i = 0; i <= splitPhrase.length - 1;i++){
+        if (words.indexOf(splitPhrase[i]) === -1){
+            return false 
+        }
+    }
+    return true 
 }
 
-console.log(minMaxProduct([6, 3, 7, 2]))
-console.log(minMaxProduct([0, 1, -5, 3, 6]))
+
+console.log(phraseFinder(['world', 'prep', 'hello', 'bootcamp'], 'bootcamp prep'))
+console.log(phraseFinder(['world', 'bootcamp', 'hello', 'prep'], 'bootcamp prep'))
+console.log(phraseFinder(['world', 'bootcamp', 'hello', 'prep'], 'hello world'))
+console.log(phraseFinder(['world', 'bootcamp', 'hello', 'prep'], 'hello prep'))
+console.log(phraseFinder(['world', 'bootcamp', 'hello', 'prep'], 'hello goodbye') )
