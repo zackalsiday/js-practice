12ce74c049dacba12e4b392c97559a71dba6c90b
zack alsiday <alsidayzack@gmail.com>
2022-12-08 12:39:10 -0800

    completed pwer

--- practice.js
+++ practice.js
@@ -2485,3 +2485,30 @@ threeIncreasing([7, 2, 4, 5, 2, 1, 6]); // => false
 // console.log(threeIncreasing([7, 2, 4, 5, 2, 1, 6]))
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
+
+/***********************************************************************
+Write a function `power(base, exp)` that takes in two numbers, a base
+and exponent. The function should return `base` raised to `exp` power.
+Solve this using a loop.
+For example, power(2, 5) is 2 raised to the 5th power,
+which is 2 * 2 * 2 * 2 * 2 = 32
+Examples:
+power(2, 5); // => 32
+power(2, 10); // => 1024
+power(9, 2); // => 81
+power(9, 3); // => 729
+power(11, 0); // => 1
+power(11, 1); // => 11
+***********************************************************************/
+
+function power(base, exp) {
+  let final = 1;
+  for (let i = 1; i <= exp; i++){
+    final *= base
+  }
+  return final 
+}
+
+console.log((11, 1))
+
+/**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
