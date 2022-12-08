641b33ab05a16eccd07e7acc61a259fc253d2778
zack alsiday <alsidayzack@gmail.com>
2022-12-08 12:25:28 -0800

    completed three increasting

--- practice.js
+++ practice.js
@@ -2446,16 +2446,42 @@ threeInARow([4, 3, 7, 7, 7, 13, 8]); // => true;
 threeInARow([10, 9, 20, 33, 3, 3]); // => false;
 ***********************************************************************/
 
-function threeInARow(arr) {
-  let counter = 0;
-  for (let i = 0; i <= arr.length -1; i++){
-    if (arr[i] === arr[i + 1] && arr[i + 1] === arr[i + 2]){
-      return true 
-    }
-  }
-  return false 
-}
+// function threeInARow(arr) {
+//   let counter = 0;
+//   for (let i = 0; i <= arr.length -1; i++){
+//     if (arr[i] === arr[i + 1] && arr[i + 1] === arr[i + 2]){
+//       return true 
+//     }
+//   }
+//   return false 
+// }
 
-console.log(threeInARow([10, 9, 20, 33, 3, 3]))
+// console.log(threeInARow([10, 9, 20, 33, 3, 3]))
+
+/**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
+
+/***********************************************************************
+Write a function `threeIncreasing(arr)` that takes in an array of
+numbers and returns true if the array contains 3 consecutive numbers in
+increasing order. The function should return false otherwise.
+Note that the 3 consecutive numbers should be increasing by 1, so the
+the second  number is 1 greater than the first, and the third number is
+1 greater  than the second.
+Examples:
+threeIncreasing([3, 2, 11, 12, 13, 2, 4]); // => true
+threeIncreasing([7, 2, 4, 5, 2, 1, 6]); // => false
+***********************************************************************/
+
+// function threeIncreasing(arr) {
+//   let counter = 0;
+//   for (let i = 0; i <= arr.length -1;i++){
+//     if (arr[i] + 1 === arr[i + 1] && arr[i + 1] + 1 === arr[i + 2]){
+//       return true 
+//     }
+//   }
+//   return false 
+// }
+
+// console.log(threeIncreasing([7, 2, 4, 5, 2, 1, 6]))
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
