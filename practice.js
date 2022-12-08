3f2767d45e89293b80efd2919563de8ebc19597b
zack alsiday <alsidayzack@gmail.com>
2022-12-08 12:22:06 -0800

    completed threeInARow

--- practice.js
+++ practice.js
@@ -2434,3 +2434,28 @@ objectSize(obj2); // => 3
 // console.log(objectSize(obj2))
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
+
+
+
+/***********************************************************************
+Write a function `threeInARow(arr)` that takes in an array of numbers
+and returns true if the array contains 3 of the same number
+consecutively. The function should return false otherwise.
+Examples:
+threeInARow([4, 3, 7, 7, 7, 13, 8]); // => true;
+threeInARow([10, 9, 20, 33, 3, 3]); // => false;
+***********************************************************************/
+
+function threeInARow(arr) {
+  let counter = 0;
+  for (let i = 0; i <= arr.length -1; i++){
+    if (arr[i] === arr[i + 1] && arr[i + 1] === arr[i + 2]){
+      return true 
+    }
+  }
+  return false 
+}
+
+console.log(threeInARow([10, 9, 20, 33, 3, 3]))
+
+/**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
